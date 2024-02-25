f0d8dc61187ae9dc49a7eca0f211e914cd8556a4
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2024-02-25 22:29:44 +0000

    test updates

--- drivers-license/javalin-record.asm
+++ drivers-license/javalin-record.asm
@@ -1,10 +1,14 @@
+///
 // Javelin: Write a program to input attempts on the World Javelin record (98m).
 // If an attempt is greater than the current record,the attempt should be stored 
 // as the new record. Enter 0 to end and output the current record.
+///
 lbUserInput			INP
 					BRZ lbProgramEnd
-					STA varAttempt
+					BRP lbTest
+					BRA lbUserInput
 
+lbTest				STA varAttempt
 					SUB varCurrentRecord
 					BRP lbNewRecord
 					BRA lbUserInput
@@ -13,8 +17,8 @@ lbNewRecord			LDA varAttempt
 					STA varCurrentRecord
 					OUT
 					BRA lbUserInput
+
 lbProgramEnd		HLT
 
 varCurrentRecord	DAT 98
 varAttempt			DAT 0
-
---
+++ drivers-license/sprint-record.asm
@@ -0,0 +1,24 @@
+///
+// The world Record for 100m sprint is 10 seconds: Write a program to override this 
+// record if a new record is set (i.e. time less than 10 seconds).
+// Enter 0 to end and output the current record.
+///
+lbUserInput			INP
+					BRZ lbProgramEnd
+					BRP lbTest
+					BRA lbUserInput
+
+lbTest				STA varAttempt
+					SUB varCurrentRecord
+					BRZ lbUserInput
+					BRP lbUserInput
+
+lbNewRecord			LDA varAttempt
+					STA varCurrentRecord
+					OUT
+					BRA lbUserInput
+
+lbProgramEnd		HLT
+
+varCurrentRecord	DAT 10
+varAttempt			DAT 0
